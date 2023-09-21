6479c6c47e80b5d7ecc7e747f8e1d1a17a11068e
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-09-21 07:47:07 +0100

    x

---
+++ demo/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    
+class TestModel(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    email = models.EmailField()
